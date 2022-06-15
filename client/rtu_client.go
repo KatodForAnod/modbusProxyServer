5db4d38bc56fbeeb7158195574ceba861362ba24
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-06-15 11:39:27 +0300

    added new funcs for rtu api

--- client/rtu_client.go
+++ client/rtu_client.go
@@ -8,7 +8,7 @@ import (
 
 type RTUClient struct {
 	handler *modbus.RTUClientHandler
-	client modbus.Client
+	client  modbus.Client
 }
 
 func (c *RTUClient) Connect(conf serial.Config, comPort string) error {
@@ -26,6 +26,16 @@ func (c *RTUClient) Connect(conf serial.Config, comPort string) error {
 	return nil
 }
 
+func (c *RTUClient) Disconnect() error {
+	log.Println("Disconnecting from port:", c.handler.Config.Address)
+	if err := c.handler.Close(); err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
 func (c *RTUClient) ReadCoils(address, quantity uint16) ([]byte, error) {
 	log.Println("ReadCoils address:", address, "quantity:", quantity)
 
@@ -38,7 +48,7 @@ func (c *RTUClient) ReadCoils(address, quantity uint16) ([]byte, error) {
 	return result, nil
 }
 
-func (c *RTUClient) WriteSingleCoil (address, value uint16) ([]byte, error)  {
+func (c *RTUClient) WriteSingleCoil(address, value uint16) ([]byte, error) {
 	log.Println("WriteSingleCoil address:", address, "value:", value)
 
 	result, err := c.client.WriteSingleCoil(address, value)
@@ -50,7 +60,7 @@ func (c *RTUClient) WriteSingleCoil (address, value uint16) ([]byte, error)  {
 	return result, nil
 }
 
-func (c *RTUClient) WriteMultipleCoils (address, quantity uint16, coils []CoilType) ([]byte, error)  {
+func (c *RTUClient) WriteMultipleCoils(address, quantity uint16, coils []CoilType) ([]byte, error) {
 	log.Println("WriteMultipleCoils address:", address, "quantity:", quantity, "coils:", coils)
 
 	var outCoils []byte
@@ -67,5 +77,119 @@ func (c *RTUClient) WriteMultipleCoils (address, quantity uint16, coils []CoilTy
 	return result, nil
 }
 
+func (c *RTUClient) WriteSingleRegister(address, value uint16) ([]byte, error) {
+	log.Println("WriteSingleRegister address:", address, "value:", value)
 
+	result, err := c.client.WriteSingleRegister(address, value)
+	if err != nil {
+		log.Println(err)
+		return []byte{}, err
+	}
 
+	return result, nil
+}
+
+func (c *RTUClient) WriteMultipleRegisters(address, quantity uint16, values []uint16) ([]byte, error) {
+	log.Println("WriteMultipleRegisters address:", address, "quantity:", quantity, "values", values)
+
+	bytes, err := c.uintToHexDecimalInBytes(values)
+	if err != nil {
+		log.Println(err)
+		return []byte{}, err
+	}
+
+	result, err := c.client.WriteMultipleRegisters(address, quantity, bytes)
+	if err != nil {
+		log.Println(err)
+		return []byte{}, err
+	}
+
+	return result, nil
+}
+
+func (c *RTUClient) ReadDiscreteInputs(address, quantity uint16) ([]byte, error) {
+	log.Println("ReadDiscreteInputs address:", address, "quantity:", quantity)
+
+	result, err := c.client.ReadDiscreteInputs(address, quantity)
+	if err != nil {
+		log.Println(err)
+		return []byte{}, err
+	}
+
+	return result, nil
+}
+
+func (c *RTUClient) ReadInputRegisters(address, quantity uint16) ([]byte, error) {
+	log.Println("ReadInputRegisters address:", address, "quantity:", quantity)
+
+	result, err := c.client.ReadInputRegisters(address, quantity)
+	if err != nil {
+		log.Println(err)
+		return []byte{}, err
+	}
+
+	return result, nil
+}
+
+func (c *RTUClient) ReadHoldingRegisters(address, quantity uint16) ([]byte, error) {
+	log.Println("ReadHoldingRegisters address:", address, "quantity:", quantity)
+
+	result, err := c.client.ReadHoldingRegisters(address, quantity)
+	if err != nil {
+		log.Println(err)
+		return []byte{}, err
+	}
+
+	return result, nil
+}
+
+func (c *RTUClient) ReadWriteMultipleRegisters(readAddress uint16, readQuantity uint16,
+	writeAddress uint16, writeQuantity uint16, values []uint16) ([]byte, error) {
+	log.Println("ReadWriteMultipleRegisters readAddress:", readAddress,
+		"readQuantity:", readQuantity, "writeAddress:", writeAddress,
+		"values:", values)
+
+	bytes, err := c.uintToHexDecimalInBytes(values)
+	if err != nil {
+		log.Println(err)
+		return []byte{}, err
+	}
+
+	result, err := c.client.ReadWriteMultipleRegisters(readAddress, readQuantity,
+		writeAddress, writeQuantity, bytes)
+	if err != nil {
+		log.Println(err)
+		return []byte{}, err
+	}
+
+	return result, nil
+}
+
+func (c *RTUClient) ReadFIFOQueue(address uint16) ([]byte, error) {
+	log.Println("ReadFIFOQueue address:", address)
+
+	result, err := c.client.ReadFIFOQueue(address)
+	if err != nil {
+		log.Println(err)
+		return []byte{}, err
+	}
+
+	return result, nil
+}
+
+func (c *RTUClient) MaskWriteRegister(address uint16, andMask uint16, orMask uint16) ([]byte, error) {
+	log.Println("MaskWriteRegister address:", address,
+		"andMask:", andMask, "orMask:", orMask)
+
+	result, err := c.client.MaskWriteRegister(address, andMask, orMask)
+	if err != nil {
+		log.Println(err)
+		return []byte{}, err
+	}
+
+	return result, nil
+}
+
+func (c *RTUClient) uintToHexDecimalInBytes(values []uint16) ([]byte, error) {
+	return []byte{}, nil
+}
--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module modbusprottocol
 
 go 1.17
 
-require github.com/goburrow/modbus v0.1.0
-
-require github.com/goburrow/serial v0.1.0 // indirect
+require (
+	github.com/goburrow/modbus v0.1.0
+	github.com/goburrow/serial v0.1.0
+)
