363cb461a879062667051e6a2815cd60ab84db25
KatodForAnod <nikita.makarchuck@yandex.by>
2022-06-17 09:27:02 +0300

    main sample

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"github.com/goburrow/serial"
 	"log"
 	"modbusprottocol/client"
+	"modbusprottocol/controller"
+	"modbusprottocol/memory"
 	"time"
 )
 
 func main() {
 	log.SetFlags(log.Lshortfile)
 
-	rtuClient := client.BaseClient{}
+	rtuClient := client.RTUClient{}
+	iotControll := controller.IoTsController{}
+	mem := memory.MemoryFmt{}
+	iotControll.Init(mem)
+
 	conf := serial.Config{
 		Address:  "COM3",
 		BaudRate: 115200,
@@ -22,7 +27,20 @@ func main() {
 		RS485:    serial.RS485Config{},
 	}
 
-	rtuClient.Connect(conf, 0x01)
-	fmt.Println(rtuClient.ReadCoils(0x11, 3))
-	fmt.Println(rtuClient.WriteSingleCoil(0x11, client.OFF))
+	rtuClient.Init("testDevice")
+	err := rtuClient.Connect(conf, 0x01)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	iotControll.AddIoTsClients([]client.IoTClient{&rtuClient})
+
+	iotControll.ObserveCoils("testDevice", 0x11, 3, time.Second)
+	time.Sleep(time.Second * 10)
+	rtuClient.StopObserveInform()
+	time.Sleep(time.Second * 10)
+
+	return
+	/*fmt.Println(rtuClient.ReadCoils(0x11, 3))
+	fmt.Println(rtuClient.WriteSingleCoil(0x11, client.ON))*/
 }
