9c75cf35dca626e0eacaad49b434917a572cef33
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-06-17 12:44:45 +0300

    new iot device config

--- config/config.go
+++ config/config.go
@@ -7,7 +7,18 @@ import (
 )
 
 type Config struct {
-	ProxyServerAddr string `json:"proxy_server_addr"`
+	ProxyServerAddr string      `json:"proxy_server_addr"`
+	IoTsDevices     []IotConfig `json:"iots_devices"`
+}
+
+type IotConfig struct {
+	ComPort           string `json:"com_port"`
+	BaudRate          int    `json:"baud_rate"`
+	DataBits          int    `json:"data_bits"`
+	StopBits          int    `json:"stop_bits"`
+	Parity            string `json:"parity"`
+	TimeoutSeconds    int    `json:"timeout_seconds"`
+	TypeIotConnection string `json:"type_iot_connection"`
 }
 
 const configPath = "conf.config"
